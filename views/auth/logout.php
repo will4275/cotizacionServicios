a23fb4018d064fe59872c9249329121a00230336
JVC-dev <jeremias.vladi@gmail.com>
2026-04-13 21:42:55 -0600

    hoy si creo

--- views/auth/logout.php
+++ views/auth/logout.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-// 🔥 borrar todas las variables de sesión
+//  borrar todas las variables de sesión
 $_SESSION = [];
 
-// 🔥 destruir la sesión
+//  destruir la sesión
 session_destroy();
 
-// 🔥 borrar cookie de sesión (opcional pero recomendado)
+//  borrar cookie de sesión (opcional pero recomendado)
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -17,8 +17,7 @@ if (ini_get("session.use_cookies")) {
         $params["httponly"]
     );
 }
-session_destroy();
-// 🔥 redirigir al login
+// redirigir al login
 header("Location: login.php");
 exit;
 ?>
